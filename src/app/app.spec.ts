88bae9608530c432315e3bec55a96fd17c1e502f
albert-dev <al96@hotmail.es>
2026-07-29 12:28:10 +0200

    feat: "changed app title"

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-product-catalog');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Prova de nivell Albert');
   });
 });
