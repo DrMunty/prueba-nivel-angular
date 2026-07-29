a507c2025d5aa53faa6e5db7eec4f182ffe3e9b5
albert-dev <al96@hotmail.es>
2026-07-29 11:42:32 +0200

    feat: "the component productList now only renders the product card component that contains all the card information"

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,13 +1,15 @@
-import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, input, ChangeDetectionStrategy, inject } from '@angular/core';
 import type { ProductListInterface } from '../../models/product-list-interface';
+import { ProductService } from '../../services/product-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductCard {
-  productData = input.required<ProductListInterface[]>();
+productList = inject(ProductService)
 }
--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,15 +1,12 @@
 import { Component, inject, ChangeDetectionStrategy} from '@angular/core';
-import { ProductService } from '../../services/product-service';
-import { RouterLink } from '@angular/router';
 import { ProductCard } from '../product-card/product-card';
 
 @Component({
   selector: 'app-product-list',
-  imports: [RouterLink],
+  imports: [ProductCard],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductList {
-  productList = inject(ProductService)
-}
+
+export class ProductList {}
